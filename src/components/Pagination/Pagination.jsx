9450ef3e593e59b8a438ac3ec7e39da269ba1f9e
Turdygazin Magzhan <magzhan.turdygazin@gmail.com>
2024-02-21 11:34:41 +0600

    feature: add pagination

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "./Pagination.module.css";
+
+const Pagination = ({
+  totalPages,
+  onNextPage,
+  onPrevPage,
+  onPageClick,
+  currentPage,
+}) => {
+  return (
+    <div className={styles.pagination}>
+      <button
+        className={`${styles.arrow} ${currentPage <= 1 && styles.disabled_arrow}`}
+        onClick={onPrevPage}
+      >
+        {"<"}
+      </button>
+
+      <div className={styles.list}>
+        {[...Array(totalPages)].map((_, idx) => (
+          <button
+            key={idx}
+            className={`${styles.pageNumber} ${
+              idx + 1 === currentPage && styles.disabled_number
+            }`}
+            onClick={() => onPageClick(idx + 1)}
+          >
+            {idx + 1}
+          </button>
+        ))}
+      </div>
+
+      <button
+        className={`${styles.arrow} ${
+          currentPage >= totalPages && styles.disabled_arrow
+        }`}
+        onClick={onNextPage}
+      >
+        {">"}
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
