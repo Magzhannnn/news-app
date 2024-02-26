b2c8b62431b67a9e21b7cdb254b5f83cdacc8144
Turdygazin Magzhan <magzhan.turdygazin@gmail.com>
2024-02-26 11:23:40 +0600

    feature: added refactor main page and newsByfilter

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./LatestNews.module.css";
 import BannersList from "../BannersList/BannersList";
+import { getLatestNews } from "../../api/apiNews";
+import { useFetch } from "../../hooks/useFetch";
 
-const LatestNews = ({ banners, isLoading }) => {
+const LatestNews = () => {
+  const { data, isLoading } = useFetch(getLatestNews);
+
   return (
     <section className={styles.section}>
-      {<BannersList banners={banners} isLoading={isLoading} />}
+      {<BannersList banners={data && data.news} isLoading={isLoading} />}
     </section>
   );
 };
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,39 +1,14 @@
 import React from "react";
 import styles from "./Main.module.css";
-import { getNews } from "../../api/apiNews";
-import { useDebounce } from "../../hooks/useDebounce";
-import { PAGE_SIZE } from "../../constant/constants";
-import { useFetch } from "../../hooks/useFetch";
-import { useFilters } from "../../hooks/useFilters";
 import LatestNews from "../../components/LatestNews/LatestNews";
 import NewsByFilters from "../../components/NewsByFilters/NewsByFilters";
 import Container from "../../UI/Container/Container";
 
 const Main = () => {
-  const { filters, changeFilter } = useFilters({
-    page_number: 1,
-    page_size: PAGE_SIZE,
-    category: null,
-    keywords: "",
-  });
-
-  const debouncedKeyWords = useDebounce(filters.keywords, 1500);
-
-  const { data, isLoading, error } = useFetch(getNews, {
-    ...filters,
-    keywords: debouncedKeyWords,
-  });
-
   return (
     <Container className={styles.main}>
-      <LatestNews isLoading={isLoading} banners={data && data.news} />
-
-      <NewsByFilters
-        isLoading={isLoading}
-        filters={filters}
-        changeFilter={changeFilter}
-        news={data?.news}
-      />
+      <LatestNews />
+      <NewsByFilters />
     </Container>
   );
 };
