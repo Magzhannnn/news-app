24d861592123b3d70eb46d93cd98d88e602d4c91
Turdygazin Magzhan <magzhan.turdygazin@gmail.com>
2024-02-29 12:55:06 +0600

    fixed: delete import file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header/Header";
 import Main from "./pages/Main/Main";
 import "./index.css";
-import { ThemeProvider, useTheme } from "./context/ThemeContext";
+import { useTheme } from "./context/ThemeContext";
 
 function App() {
   const { isDark } = useTheme();
