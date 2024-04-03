e7a4db1d826b2d1653bcd2a0007f821eb9c6f150
Turdygazin Magzhan <magzhan.turdygazin@gmail.com>
2024-04-03 12:35:58 +0500

    feature: added router and newsPage

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -3,12 +3,13 @@ import "@/shared/index.css";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@/app/providers/ThemeContext";
 import { store } from "@/app/appStore.ts";
-import BaseLayout from "./layouts/baseLayout";
+import { RouterProvider } from "react-router-dom";
+import { appRouter } from "./appRouter";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
     <Provider store={store}>
-      <BaseLayout />
+      <RouterProvider router={appRouter} />
     </Provider>
   </ThemeProvider>
 );
